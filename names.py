55ee638f3ad581ef4800373d67f7beb3c1cfd329
Valentine Uchenna Ukah <122880405+Tinobrace1@users.noreply.github.com>
2023-04-20 05:58:19 +0100

    adding all newly copied files

---
+++ names.py
@@ -0,0 +1,13 @@
+#This is a python thing, yeah
+
+1. Ukah Valentine Uchenna
+
+2. Emenike-Ukah Emmanuella Chioma
+
+3. Ukah Assumpta Chimuanya
+
+4. Ukah Anita Onyedikachi
+
+5. Emenike-Ukah Josephine Nkechinyere
+
+# That is all, for now though.
